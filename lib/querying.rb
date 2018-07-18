ac89764ee1cda0c63d83d48e61b3f4eaa58a1c5a
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-18 18:52:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM series WHERE species = 'human' ORDER BY COUNT(characters.species) DESC LIMIT 1"
+  "SELECT title FROM series WHERE charecters.species = 'human' ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
